d98a731ebf12ad0b9135f951b80bc182597cf7ee
Ubuntu <ubuntu@ip-172-31-17-170.ec2.internal>
2022-03-29 13:38:43 +0000

    added comments

--- guess_a_num.py
+++ guess_a_num.py
@@ -2,33 +2,43 @@
 
 # Created By: Alex De Meo
 # Date: 03/25/2022
-# Description:
+# Description: This program generates a random number that
+# the user must guess correctly to win the game
 
 import math
 import random
 import sys
 
-def main():
-    while True:
-        r_num = random.randint(1,15)
-        print("I just generated a random number between 1-15. Can you Guess it correctly?")
-        print(r_num)
 
+def main():
+    # loop to run the game again if user responds yes
+    while True:
+
+        #Creating the random number
+        r_num = random.randint(1, 15)
+        print(
+            "I just generated a random number between 1-15. Can you Guess it correctly?"
+        )
+        print(r_num)
+        
+        # Loop allows user to keep guessing until they get it right
         while True:
             u_num = int(input("Input your number below:\n"))
 
+            # Comparing user number with random number
             if u_num == r_num:
                 print("Congratulations, you guessed the number correctly!!")
                 answer = input("Would you like to play again? (y/n):").strip().lower()
 
+                # what to do with user answer
                 if answer == "y":
                     print("Okay")
                     break
                 else:
                     print("Okay")
+                    # Ends program
                     sys.exit()
-                    
-                
+
             else:
                 print("Uh oh, wrong answer. Guess again!")
 
